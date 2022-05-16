0b0a0dc19130a0dfbce2041b2de35833dc2fc62a
NueXini <924282761@qq.com>
2022-05-16 22:00:36 +0800

    refresh

--- Z.Backup/scripts/patch.sh
+++ Z.Backup/scripts/patch.sh
@@ -4,11 +4,8 @@
 
 echo '==========Star to use Patch=========='
 
-wget -O ./target/linux/ramips/dts/mt7621_d-team_newifi-d2.dts -q --no-check-certificate https://raw.githubusercontent.com/x-wrt/x-wrt/22.02_b202203130310/target/linux/ramips/dts/mt7621_d-team_newifi-d2.dts
-
 wget -O ./target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g.dts -q --no-check-certificate https://raw.githubusercontent.com/x-wrt/x-wrt/22.02_b202203130310/target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g.dts
 
-cat ./target/linux/ramips/dts/mt7621_d-team_newifi-d2.dts
 cat ./target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g.dts
 
-echo '==========End to use Patch=========='
+echo '----------End to use Patch-----------'
--- Z.Backup/sh/Hi_NueXini.sh
+++ Z.Backup/sh/Hi_NueXini.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+# echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+
+sed -i 's/192\.168\.1.*\.1/192\.168\.3\.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/%R/NueXini/g' package/base-files/files/etc/openwrt_release
+
+sed -i 's,0.openwrt.pool.ntp.org,time1.cloud.tencent.com,g' package/base-files/files/bin/config_generate
+sed -i 's,1.openwrt.pool.ntp.org,ntp1.aliyun.com,g' package/base-files/files/bin/config_generate
+sed -i 's,2.openwrt.pool.ntp.org,ntp.ntsc.ac.cn,g' package/base-files/files/bin/config_generate
+sed -i 's,3.openwrt.pool.ntp.org,cn.ntp.org.cn,g' package/base-files/files/bin/config_generate
+
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/+luci-theme-bootstrap //g' feeds/luci/collections/luci/Makefile
 
-sed -i 's/192.168.1.*.1/192.168.3.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
+sed -i 's/192\.168\.1.*\.1/192\.168\.3\.1/g' feeds/luci/modules/luci-mod-system/htdocs/luci-static/resources/view/system/flash.js
 
 # /usr/lib/lua/luci/version.lua
 sed -i 's/${3:-LuCI}/NueXini/g' feeds/luci/modules/luci-base/src/mkversion.sh
