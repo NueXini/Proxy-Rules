ba2115c8ccd8036e286b880a2390d3b71ae71bb4
NueXini <924282761@qq.com>
2022-05-25 10:32:14 +0800

    update

--- Z.Backup/sh/Hi_NueXini.sh
+++ Z.Backup/sh/Hi_NueXini.sh
@@ -3,6 +3,8 @@
 # sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 # echo 'src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
 
+# wget -O ./target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g.dts -q --no-check-certificate https://raw.githubusercontent.com/x-wrt/x-wrt/22.02_b202203130310/target/linux/ramips/dts/mt7621_xiaomi_mi-router-3g.dts
+
 sed -i 's/192\.168\.1.*\.1/192\.168\.3\.1/g' package/base-files/files/bin/config_generate
 
 sed -i 's/%R/NueXini/g' package/base-files/files/etc/openwrt_release
