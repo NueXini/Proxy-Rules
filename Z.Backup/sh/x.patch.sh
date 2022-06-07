52aada481a17f145e590c26bc2ea28e99b086af0
NueXini <924282761@qq.com>
2022-06-07 19:09:20 +0800

    update

--- Z.Backup/sh/x.patch.sh
+++ Z.Backup/sh/x.patch.sh
@@ -2,10 +2,10 @@
 # NueXini
 
 # dnsmasq: add filter-aaaa option
-# cp -f $GITHUB_WORKSPACE/Z.Backup/patches/910-dnsmasq-mini-ttl.patch ./package/network/services/dnsmasq/patches/
-# patch -p0 ./package/network/services/dnsmasq/files/dnsmasq.init $GITHUB_WORKSPACE/Z.Backup/patches/dnsmasq.init.patch
+cp -f $GITHUB_WORKSPACE/Z.Backup/patches/900-dnsmasq-filter-aaaa.patch ./package/network/services/dnsmasq/patches/
+patch -p0 ./package/network/services/dnsmasq/files/dnsmasq.init $GITHUB_WORKSPACE/Z.Backup/patches/dnsmasq.init.patch
 
-# echo '==============[dnsmasq.init]=============='
-# sed -n '933,939p' ./package/network/services/dnsmasq/files/dnsmasq.init
-# echo '--------------[dnsmasq.init]--------------'
+echo '==============[dnsmasq.init]=============='
+sed -n '933,943p' ./package/network/services/dnsmasq/files/dnsmasq.init
+echo '--------------[dnsmasq.init]--------------'
 
