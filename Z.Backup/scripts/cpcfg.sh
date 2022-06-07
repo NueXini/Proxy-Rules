aca2e34d19bcdcebbc93c3d9a35af251334105ce
NueXini <924282761@qq.com>
2022-06-07 22:11:54 +0800

    update

--- Z.Backup/scripts/cpcfg.sh
+++ Z.Backup/scripts/cpcfg.sh
@@ -17,3 +17,10 @@ case "$num" in
 	cp -f $GITHUB_WORKSPACE/Z.Backup/config/xiaomi_mi-router-3g.config .config	
 esac
 
+CONFIG_VERSION_NUMBER_NX=$(cat feeds/x/rom/lede/config.ramips-mt7621 | grep  'CONFIG_VERSION_NUMBER' | cut -d '"' -f 2)
+CONFIG_VERSION_NUMBER_NX=`echo ${CONFIG_VERSION_NUMBER_NX:0:15}`
+sed -i "s/CONFIG_VERSION_NUMBER=\".*\"/CONFIG_VERSION_NUMBER=\"$CONFIG_VERSION_NUMBER_NX\"/" .config
+
+CONFIG_VERSION_CODE_NX=$(cat feeds/x/rom/lede/config.ramips-mt7621 | grep  'CONFIG_VERSION_CODE=' | cut -d '"' -f 2)
+sed -i "s/CONFIG_VERSION_CODE=\".*\"/CONFIG_VERSION_CODE=\"$CONFIG_VERSION_CODE_NX\"/" .config
+
